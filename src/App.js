cca587d33ee97f102a47c3051080aa11724345c9
Mark Tellez <mark.tellez@icloud.com>
2018-04-20 23:36:52 -0500

    basic router implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Router from './Router'
 
 import './app.css'
+import './navigation.css'
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/cart'>Cart</NavLink></li>
+    </ul>
+  </nav>
+)
 
 export default function App() {
   return (
     <div
       className="page-container"
     >
-      <h1>Online Toy Store</h1>
+      <Navigation />
+
+      <Router />
     </div>
   )
 }
